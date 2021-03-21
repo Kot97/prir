2d8156304b6a40af5ee7893f72dc01744fb97dca
Grzegorz Król <kpv135@gmail.com>
2021-03-21 23:13:41 +0100

    clean code

--- CUDA/kernels/disarium_number.cu
+++ CUDA/kernels/disarium_number.cu
@@ -4,27 +4,18 @@ __device__ bool isNumberDisarium(unsigned long number);
 
 __device__ unsigned long pow(unsigned long x,unsigned long n);
 
-__device__ void addResult(unsigned long result);
+__device__ void addResult(unsigned long *generatedNumbersGPU, unsigned long result);
 
 __global__ void generateDisariumNumbers(unsigned long *generatedNumbersGPU, const unsigned long NUMBERS_COUNT) {
-//    __shared__ unsigned long int numbersFinded; //todo shared
-//    if (threadIdx.x == 0)
-//        numbersFinded = 0;
-//    __syncthreads();
-//    unsigned long i = 0;
     unsigned long i = blockIdx.x * blockDim.x + threadIdx.x;
     while (i < NUMBERS_COUNT) {
-        if (isNumberDisarium(i)) {
-            generatedNumbersGPU[i] = 1;
-            generatedNumbersGPU[i] = isNumberDisarium(i);
-//            numbersFinded++; //todo synchronize
-        } else
-            generatedNumbersGPU[i] = 0;
+        if (isNumberDisarium(i))
+            addResult(generatedNumbersGPU, i);
         i += blockDim.x*gridDim.x;
     }
 }
 
-__device__ void addResult(unsigned long result) {
+__device__ void addResult(unsigned long *generatedNumbersGPU, unsigned long result) {
 
 }
 
@@ -44,7 +35,6 @@ __device__ unsigned long countDigits(unsigned long number) {
         number /= 10;
         digits_count++;
     }
-
     return digits_count;
 }
 
