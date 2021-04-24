78f886ee4cd9ba05120b09f63f62322104b459cf
Grzegorz Król <kpv135@gmail.com>
2021-04-25 00:26:59 +0200

    refactor of disarium

--- CUDA/disarium.cu
+++ CUDA/disarium.cu
@@ -8,9 +8,11 @@ const unsigned int NUMBERS_COUNT = 100000;
 
 const unsigned int THREADS_NUM = 128;
 
-void printResult(const unsigned int *generatedNumbers, const bool *result);
+void runKernel(unsigned int *generatedNumbersGPU, bool *resultGPU);
 
 void generateNumbers(unsigned int *table, unsigned int n);
+
+void printResult(const unsigned int *generatedNumbers, const bool *result);
 
 int main(int argc, char **argv) {
     unsigned int *generatedNumbersCPU = new unsigned int[NUMBERS_COUNT];
@@ -21,6 +23,28 @@ int main(int argc, char **argv) {
 
     bool *resultGPU = allocateArrayOnGPU<bool>(NUMBERS_COUNT);
 
+    runKernel(generatedNumbersGPU, resultGPU);
+
+    bool *resultCPU = new bool[NUMBERS_COUNT];
+    transferDataFromGPU<bool>(resultCPU, resultGPU, NUMBERS_COUNT);
+
+    freeArrayGPU(resultGPU);
+    freeArrayGPU(generatedNumbersGPU);
+
+    printResult(generatedNumbersCPU, resultCPU);
+    delete[] generatedNumbersCPU;
+    delete[] resultCPU;
+
+    return 0;
+}
+
+void generateNumbers(unsigned int *table, unsigned int n) {
+    srand(time(NULL));
+    for (unsigned int i = 0; i < n; i++)
+        table[i] = rand();
+}
+
+void runKernel(unsigned int *generatedNumbersGPU, bool *resultGPU) {
     unsigned int blocksNumber = getBlocksNumber(THREADS_NUM, NUMBERS_COUNT);
     std::cout << "block num: " << blocksNumber << std::endl << "threads count: " << THREADS_NUM << std::endl;
 
@@ -42,18 +66,6 @@ int main(int argc, char **argv) {
 
     cudaEventDestroy(start);
     cudaEventDestroy(stop);
-
-    bool *resultCPU = new bool[NUMBERS_COUNT];
-    transferDataFromGPU<bool>(resultCPU, resultGPU, NUMBERS_COUNT);
-
-    freeArrayGPU(resultGPU);
-    freeArrayGPU(generatedNumbersGPU);
-
-    printResult(generatedNumbersCPU, resultCPU);
-    delete[] generatedNumbersCPU;
-    delete[] resultCPU;
-
-    return 0;
 }
 
 void printResult(const unsigned int *generatedNumbers, const bool *result) {
@@ -62,10 +74,4 @@ void printResult(const unsigned int *generatedNumbers, const bool *result) {
         if (result[i])
             std::cout << generatedNumbers[i] << std::endl;
     std::cout << "----------" << std::endl;
-}
-
-void generateNumbers(unsigned int *table, unsigned int n) {
-    srand(time(NULL));
-    for (unsigned int i = 0; i < n; i++)
-        table[i] = rand();
 }
